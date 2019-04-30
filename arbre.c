9ba5f36aa5243585f73a6d861e522e0aec7de60b
valentin leclere <48153731+valeclere@users.noreply.github.com>
2019-04-30 15:14:22 +0200

    tests des fonctions de bases

--- arbre.c
+++ arbre.c
@@ -53,46 +53,13 @@ void AjoutHorizontale(Noeud_t * GrandFrere, Noeud_t * PetitFrere)
 	}
 }
 
-/*
-Arbre_t CreationArbre(char ** listeArbre, int taille)
-{
-	//noeud i ===  [arbre[i][0],arbre[i][1],arbre[i][2]] === [pere,fils,frere]
-	
-	int i;
-	Arbre_t arbre = NULL; // arbre à retourner
-	Noeud_t * cour = NULL; //pointeur qui contient le dernier noeud traiter pour chainer
 
-	Noeud_t * pere = NULL, * fils = NULL, * frere = NULL;
-	pere = InitNoeud(listeArbre[0][0]);
-	fils = InitNoeud(listeArbre[0][1]);
-	frere = InitNoeud(listeArbre[0][2]);
-	
-	AjoutVerticale(pere, fils);
-	AjoutHorizontale(pere, frere);
-	
-	arbre = pere;
-	cour = pere;
-	
-	free(pere);
-	free(fils);
-	free(frere);
-	
-	
-	
-	
-	for (i=1;i<taille;i++) // i = 0 déjà traiter pour la racine de l'arbre
-	{
-		Noeud_t * pere = NULL, * fils = NULL, * frere = NULL;
-		pere = InitNoeud(listeArbre[i][0]);
-		fils = InitNoeud(listeArbre[i][1]);
-		frere = InitNoeud(listeArbre[i][2]);
-		
-		AjoutVerticale(pere, fils);
-		AjoutHorizontale(pere, frere);
-				
-		free(pere);
-		free(fils);
-		free(frere);
-	}
-}
-*/
+
+
+
+
+
+
+
+
+
--- main.c
+++ main.c
@@ -2,10 +2,24 @@
 
 int main()
 {
-	//declarer arbre (pt sur Noeud)
-	//declarer racine (Noeud)
-	printf("le tp3 commence!!\n");
-	 
+	/*-----TESTS fonctions de base (InitNoeud, AjoutVerticale, AjoutHorizontale)----*/
+	Arbre_t arbre;
+	
+	arbre = InitNoeud('a');
+	Noeud_t * fils = InitNoeud('B');
+	Noeud_t * frere = InitNoeud('C');
+	AjoutVerticale(arbre, fils);
+	AjoutHorizontale(fils, frere);
+	
+	printf("mot 1 = %c%c \n mot 2 = %c%c\n", arbre->lettre, arbre->lv->lettre, arbre->lettre, arbre->lv->lh->lettre);
+	
+	printf("l'arbre est cree!!\n");
+	
+	free(arbre);
+	free(fils);
+	free(frere);
+	/*---------FIN TESTS----------*/
+	
 	
 	return 0;
 }
