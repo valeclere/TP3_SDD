a8745c0a2400bdb009efe6f248b86619d2ae21b3
Pereira Loann <33416152+LoannPereira@users.noreply.github.com>
2019-05-28 15:25:00 +0200

    début fonctions de tests

---
+++ test.c
@@ -0,0 +1,27 @@
+#include "test.h"
+
+void test_Charger_Mots(){
+	printf("------Test de la fonction ChargerMots-------\n");
+}
+
+void test_Insertion(){
+	printf("------Test de la fonction Insertion-------\n");
+	
+}
+
+void test_Rechercher(){
+	printf("------Test de la fonction Rechercher-------\n");
+}
+
+void test_Recherche_Dico(){
+	printf("------Test de la fonction RechercheDico-------\n");
+
+}
+
+void test_Affichage(){
+	printf("------Test de la fonction Affichage-------\n");
+}
+
+void test_Affichage_Motif(){
+	printf("------Test de la fonction AffichageMotif-------\n");
+}
---
+++ test.h
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h> 
+
+void test_Charger_Mots();
+void test_Insertion();
+void test_Rechercher();
+void test_Recherche_Dico();
+void test_Affichage();
+void test_Affichage_Motif();
